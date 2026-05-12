3fcd150431b37213782bf3273848bab2b8e06206
SONY <prince56studio@gmail.com>
2026-05-12 22:44:10 +0530

    Updates

--- assets/react-wQPLqj5n.js
+++ assets/react-wQPLqj5n.js
@@ -1,4 +1,4 @@
-import{n as e,r as t}from"./index-BfiFizTy.js";import{n}from"./hls-CkTAylTf.js";import{n as r,t as i}from"./dist-LeT_49db.js";var a=t(e(),1),o=(e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{let{src:r,...i}=t;return`
+import{n as e,r as t}from"./index-1PEvf60L.js";import{n}from"./hls-CkTAylTf.js";import{n as r,t as i}from"./dist-LeT_49db.js";var a=t(e(),1),o=(e=>class extends e{static shadowRootOptions={...e.shadowRootOptions};static getTemplateHTML=(t,n={})=>{let{src:r,...i}=t;return`
         <script type="application/json" id="config">
           ${JSON.stringify(n.config||{})}
         <\/script>
